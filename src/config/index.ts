9150f2a88a46b7cf6da59d1957d642b303f5d0cf
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-06-30 02:55:20 +0530

    feat: rainbow kit config

---
+++ src/config/index.ts
@@ -0,0 +1,40 @@
+import { getDefaultWallets, getDefaultConfig } from "@rainbow-me/rainbowkit"
+import {
+    zerionWallet,
+    rainbowWallet,
+    trustWallet,
+    ledgerWallet,
+} from "@rainbow-me/rainbowkit/wallets"
+import {
+    baseSepolia,
+    localhost,
+} from "wagmi/chains"
+
+const { wallets } = getDefaultWallets()
+
+export const WALLETCONNECT_PROJECT_ID =
+    process.env.NEXT_PUBLIC_PROJECT_ID ?? ""
+if (!WALLETCONNECT_PROJECT_ID) {
+    console.warn(
+        "You need to provide a NEXT_PUBLIC_PROJECT_ID env variable"
+    )
+}
+export const config = getDefaultConfig({
+    appName: "RainbowKit demo",
+    projectId: WALLETCONNECT_PROJECT_ID,
+    wallets: [
+        ...wallets,
+        {
+            groupName: "Other",
+            wallets: [zerionWallet, rainbowWallet, trustWallet, ledgerWallet],
+        },
+    ],
+    chains: [
+        baseSepolia,
+        localhost,
+        ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
+            ? [baseSepolia]
+            : []),
+    ],
+    ssr: true,
+})
