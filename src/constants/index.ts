69e84ed3c57d337df369b5864b75315dc370ffbb
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-06-30 04:41:39 +0530

    feat: constant

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+import { abi } from "./abi";
+import { BASE_SEPOLIA } from "./address";
+
+export const config = {
+    "base-sepolia": {
+        address: BASE_SEPOLIA,
+        abi,
+    },
+};
+
