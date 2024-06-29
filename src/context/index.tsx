2547831ea0004aefa48111fa77d2f73bbfd6786c
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-06-30 01:12:30 +0530

    feat: web3 provider

---
+++ src/context/index.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { config } from "@/config";
+
+const queryClient = new QueryClient();
+
+export default function Web3Provider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider
+          theme={darkTheme({
+            accentColor: "#12AAff",
+            accentColorForeground: "#213147",
+            overlayBlur: "small",
+          })}
+        >
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
