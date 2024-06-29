8b54e1e87c0fdd9fc715baa244ac23cbffd4792f
HAPPYS1NGH <harpreetsingh140503@gmail.com>
2024-06-29 22:50:07 +0530

    feat: header footer

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="absolute bottom-0 ">
+      <footer className=" bg-blue max-w-fit p-2 text-xs text-[#fff] text-center ">
+        Support with Eth - 0x15413cd3Bb0d8BCB88a70aE3679f68Dd7E5194Fb
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
